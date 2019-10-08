5e8f283005bd05878b9bd2370de71b4b7ce338e5
xiehongwei <283994917@qq.com>
2019-10-08 17:35:00 +0800

    mock 数据

--- .umirc.js
+++ .umirc.js
@@ -22,11 +22,11 @@ export default {
       },
     }],
   ],
-  "proxy": {
-    "/api": {                                       //---step1 设置了需要代理的请求头，比如这里定义了 /api ，当你访问如 /api/abc 这样子的请求，就会触发代理
-      "target": "https://pvp.qq.com/web201605/js/", //---step2 设置代理的目标，即真实的服务器地址
-      "changeOrigin": true,                         //---step3 设置是否跨域请求资源
-      "pathRewrite": { "^/api" : "" }               //---step4 表示是否重写请求地址，比如这里的配置，就是把 /api 替换成空字符
-    }
-  }
+  // "proxy": {
+  //   "/api": {                                       //---step1 设置了需要代理的请求头，比如这里定义了 /api ，当你访问如 /api/abc 这样子的请求，就会触发代理
+  //     "target": "https://pvp.qq.com/web201605/js/", //---step2 设置代理的目标，即真实的服务器地址
+  //     "changeOrigin": true,                         //---step3 设置是否跨域请求资源
+  //     "pathRewrite": { "^/api" : "" }               //---step4 表示是否重写请求地址，比如这里的配置，就是把 /api 替换成空字符
+  //   }
+  // }
 }
---
+++ mock/heros.js
@@ -0,0 +1,5 @@
+import heroList from './herolist';
+
+export default {
+  '/api/herolist.json': heroList
+};
