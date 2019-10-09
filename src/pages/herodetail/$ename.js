2faef7b64fa015707db031d0deb84ebbc90901b0
xiehongwei <283994917@qq.com>
2019-10-09 08:48:14 +0800

    动态路由

---
+++ src/pages/herodetail/$ename.js
@@ -0,0 +1,13 @@
+
+import styles from './ename.css';
+
+export default function({match}) {
+
+  console.log(match);
+
+  return (
+    <div className={styles.normal}>
+      <h1>herodetail Page ename</h1>
+    </div>
+  );
+}
